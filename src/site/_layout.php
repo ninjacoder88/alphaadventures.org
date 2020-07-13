b17319efbdb98bdf53a35072bb01a316b934af7d
Tommy Harris <tomharris@idtdna.com>
2020-07-13 11:31:11 -0500

    init

---
+++ src/site/_layout.php
@@ -0,0 +1,30 @@
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="css/lib/boostrap-4.5.0.min.css"/>
+        <script type="text/javascript" src="js/lib/require-2.3.6.js"></script>
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">Alpha Adventures</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-item nav-link" href="#">Calendar</a>
+                    <a class="nav-item nav-link" href="#">Something</a>
+                    <a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Something else</a>
+                </div>
+            </div>
+        </nav>
+
+        <div class="container">
+            <div class="jumbotron">
+                <h1>Alpha Adventures</h1>
+            </div><!--/jumbotron-->
+        </div><!--/container-->
+    </body>
+</html>
---
+++ src/site/js/requireConfig.js
@@ -0,0 +1,8 @@
+require.config({
+    baseUrl: "/js/lib",
+    paths: {
+        "jquery": "jquery-3.5.1.min",
+        "bootstrap": "boostrap.bundle-4.5.0.min",
+        "knockout": "knockout-3.5.0min"
+    }
+});
